f1cac832ae26513e6da2aa1b28f9e55be4b76c1b
HariGadi123 <hari@g10consultancy.com>
2026-06-05 12:12:55 +0530

    project setup is initialised

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PasswordValidator from "./Components/PasswordValidator/PasswordValidator";
+
+function App() {
+  return (
+    <>
+      <PasswordValidator />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/PasswordValidator/PasswordValidator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PasswordValidator: React.FC = () => {
+  return <div>PasswordValidator</div>;
+};
+
+export default PasswordValidator;
